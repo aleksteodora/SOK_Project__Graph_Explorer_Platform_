3ceaa7dbc3fdbc58a57cf0c6dc97168976282371
Vukasin Vujovic <vujovic.sv11.2023@uns.ac.rs>
2026-03-15 02:27:44 +0100

    #37 add function to api __init_-

--- api/src/api/__init__.py
+++ api/src/api/__init__.py
@@ -1,4 +1,4 @@
-from .model.node import Node, AttributeValue, validate_attribute_value
+from .model.node import Node, AttributeValue, validate_attribute_value, parse_attribute_value
 from .model.edge import Edge
 from .model.graph import Graph
 from .plugin.plugin import PluginParameter, DataSourcePlugin, VisualizerPlugin
@@ -9,6 +9,7 @@ __all__ = [
     "Graph",
     "AttributeValue",
     "validate_attribute_value",
+    "parse_attribute_value",
     "PluginParameter",
     "DataSourcePlugin",
     "VisualizerPlugin",
